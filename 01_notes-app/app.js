56c92174c35e5d7e95a3e9d47e5cdf41d09e9ce4
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-15 14:24:47 -0600

    import function from another file

--- 01_notes-app/app.js
+++ 01_notes-app/app.js
@@ -1,7 +1,9 @@
 'use-strict';
 const fs = require('fs');
+const notes = require('./notes');
 
 const FILE_PATH = __dirname + '\\' + 'notes.txt';
 
-fs.writeFileSync(FILE_PATH, 'This file was created by nodejs');
-fs.appendFileSync(FILE_PATH, '\n this is another line');
+console.log(notes.getNotes());
+// fs.writeFileSync(FILE_PATH, 'This file was created by nodejs');
+// fs.appendFileSync(FILE_PATH, '\n this is another line');
---
+++ 01_notes-app/notes.js
@@ -0,0 +1,6 @@
+'use-strict';
+function getNotes() {
+    return 'Your notes';
+};
+
+exports.getNotes = getNotes;
