137e1e08be12c9979a99cc5ae03c40f28ebddc74
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-16 12:14:19 -0600

    playground: simplification

--- playground/01-json.js
+++ playground/01-json.js
@@ -2,10 +2,6 @@
 
 const fs = require('fs');
 
-const fileBuffer = fs.readFileSync('01-data.json');
-const fileData = fileBuffer.toString();
-
-const fileObj = JSON.parse(fileData);
-fileObj.name = 'Alessa';
-fileObj.age = 34;
+let fileObj = JSON.parse(fs.readFileSync('01-data.json').toString());
+fileObj = { ...fileObj, name: 'Alessa', age: 34 };
 console.log(fileObj);
