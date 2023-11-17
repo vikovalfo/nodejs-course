a43b7748691d0ff00bbc8ec69d8faccd56ae76a7
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-17 15:19:53 -0600

    playground: callbacks

---
+++ playground/04_callbacks.js
@@ -0,0 +1,23 @@
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+        };
+        callback(data);
+    }, 2000);
+};
+
+geocode('Philadelphia', (data) => {
+    console.log(data);
+});
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 2000);
+};
+
+add(1, 4, (sum) => {
+    console.log(sum);
+});
