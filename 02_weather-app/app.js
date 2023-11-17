ce1f948abd56b4c6a984da93f7d1576dbce9af76
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-17 12:57:09 -0600

    another api request added, and error handling

--- 02_weather-app/app.js
+++ 02_weather-app/app.js
@@ -1,13 +1,26 @@
 'use-strict';
+const request = require('request');
 
 const { config } = require('./config');
 
-const fetchCurrentWeather = async () => {
-    const response = await fetch(`${config.API_HOST}${config.CURRENT_WEATHER_PATH}?access_key=${config.ACCESS_KEY}&query=${config.LOCATION}`);
-    if (response.ok) {
-        const responseData = await response.json();
-        console.log(responseData.current);
-    }
+const weatherstackURL = `${config.weatherstack.API_HOST}${config.weatherstack.CURRENT_PATH}?access_key=${config.weatherstack.ACCESS_KEY}&query=${config.weatherstack.LOCATION}`;
+const mapboxURL = `${config.mapbox.API_HOST}${config.mapbox.PLACES_PATH}${config.mapbox.LOCATION}?access_token=${config.mapbox.ACCESS_KEY}&limit=${config.mapbox.LIMIT}`;
+
+const weatherApp = async () => {
+    await request({ url: weatherstackURL, json: true }, (error, response) => {
+        if (response.statusCode === 200) {
+            const current = response.body['current'];
+            console.log(`It\'s currently ${current.temperature} degrees out there, is a ${current.precip}% chances of rain.`);
+        } else console.log(response.statusCode, response.statusMessage);
+    });
+
+    await request({ url: mapboxURL, json: true }, (error, response) => {
+        if (response.statusCode === 200) {
+            const latitude = response.body.features[0].center[0];
+            const longitude = response.body.features[0].center[1];
+            console.log(latitude, longitude);
+        } else console.log(response.statusCode, response.statusMessage);
+    });
 };
 
-fetchCurrentWeather();
+weatherApp();
--- 02_weather-app/config.js
+++ 02_weather-app/config.js
@@ -1,8 +1,17 @@
 const config = {
-    CURRENT_WEATHER_PATH: 'current',
-    API_HOST: 'http://api.weatherstack.com/',
-    ACCESS_KEY: '9e709c4093d62f6a72bdfec12424c2e1',
-    LOCATION: 'Guadalajara'
+    weatherstack: {
+        CURRENT_PATH: 'current',
+        API_HOST: 'http://api.weatherstack.com/',
+        ACCESS_KEY: '9e709c4093d62f6a72bdfec12424c2e1',
+        LOCATION: 'Guadalajara'
+    },
+    mapbox: {
+        API_HOST: 'https://api.mapbox.com/geocoding/v5/',
+        PLACES_PATH: 'mapbox.places/',
+        LOCATION: 'Los%20Angeles.json',
+        ACCESS_KEY: 'pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw',
+        LIMIT: '1'
+    }
 };
 
 exports.config = config;
