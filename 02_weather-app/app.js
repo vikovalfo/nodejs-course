68ec369720e6d15bc9ce5f5bd9a07a0856e69265
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-17 19:50:51 -0600

    callback abstract challenge

--- 02_weather-app/app.js
+++ 02_weather-app/app.js
@@ -1,8 +1,13 @@
 'use-strict';
+const { forecast } = require('./utils/forecast');
+/* const { geocode } = require('./utils/geocode');
 
-const geocode = require('./utils/geocode');
+geocode('Philadelphia', (error, data) => {
+    if (error) console.log('Error', error);
+    if (data) console.log('Data', data);
+}); */
 
-geocode.geocode('Philadelphia', (error, data) => {
+forecast(-75.7088, 44.1545, (error, data) => {
     if (error) console.log('Error', error);
     if (data) console.log('Data', data);
 });
--- 02_weather-app/utils/forecast.js
+++ 02_weather-app/utils/forecast.js
@@ -2,27 +2,21 @@ const request = require('request');
 
 const { config } = require('../config/config');
 
-const mapboxURL = `${config.mapbox.API_HOST}${config.mapbox.PLACES_PATH}${config.mapbox.LOCATION}?access_token=${config.mapbox.ACCESS_KEY}`;
 
 const forecast = async (latitude, longitude, callback) => {
-    await request({ url }, (error, response) => {
-
+    const url = `${config.weatherstack.API_HOST}${config.weatherstack.CURRENT_PATH}?access_key=${config.weatherstack.ACCESS_KEY}&query=${latitude},${longitude}`;
+    console.log(url);
+    await request({ url, json: true }, (error, response) => {
         if (error) {
-            callback('Unable to connect to location services!', undefined);
-        } else if (JSON.parse(response.body).features.length === 0) {
-            callback('Unable to find location, try another search...', undefined);
+            callback('Unable to connecto to weather services', undefined);
+        } else if (response.body.error) {
+            callback(response.body.error.info, undefined);
         } else {
-            const features = JSON.parse(response.body).features;
-            callback(undefined, {
-                latitude: features[0].center[0],
-                longitude: features[0].center[1],
-                location: features[0].place_name
-            })
+            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.");
         }
     });
 };
 
 module.exports = {
-    geocode,
-    weatherApp
+    forecast
 };
