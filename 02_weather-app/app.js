3f39f1000057db84b0f15cb0d426cc4cedba554a
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-17 21:25:25 -0600

    accepting arguments

--- 02_weather-app/app.js
+++ 02_weather-app/app.js
@@ -2,14 +2,17 @@
 const { forecast } = require('./utils/forecast');
 const { geocode } = require('./utils/geocode');
 
-geocode('Guadalajara', (geocodeError, geocodeData) => {
-    if (geocodeError) return console.log(geocodeError);
-    if (geocodeData) {
-        forecast(geocodeData.latitude, geocodeData.longitude, (forecastError, forecastData) => {
-            if (forecastError) return console.log('Error', forecastError);
-            console.log(forecastData);
-            console.log(geocodeData.location);
-        });
-    }
-}); 
+const location = process.argv[2];
+if (location) {
+    geocode(location, (geocodeError, geocodeData) => {
+        if (geocodeError) return console.log(geocodeError);
+        if (geocodeData) {
+            forecast(geocodeData.latitude, geocodeData.longitude, (forecastError, forecastData) => {
+                if (forecastError) return console.log('Error', forecastError);
+                console.log(forecastData);
+                console.log(geocodeData.location);
+            });
+        }
+    });
+} else console.log('Please provide a location as argument!');
 
