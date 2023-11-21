e0130eee23d153fb085faf248a14fe8da5f196ae
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-21 16:21:59 -0600

    error handling

--- 03_web-server/src/app.js
+++ 03_web-server/src/app.js
@@ -29,8 +29,12 @@ app.get('/about', (_req, res) => {
     res.render('about', { title: 'About', name: 'Alessa' });
 });
 
-app.get('/help', (req, res) => {
+app.get('/help', (_req, res) => {
     res.render('help', { title: 'Help', message: 'May I help you?' });
 });
+
+app.get('/help/*', (_req, res) => res.render('error', { title: '404 for help', message: 'Help article not found' }));
+
+app.get('*', (_req, res) => res.render('error', { title: '404', message: 'Page not found' }));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
