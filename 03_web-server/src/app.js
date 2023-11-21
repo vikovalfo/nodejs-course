2b51e80102e28d353dbf675b12f2b84917d1f650
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-21 15:59:47 -0600

    header and footers using hbs partials

--- 03_web-server/src/app.js
+++ 03_web-server/src/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const hbs = require('hbs');
 
 const publicDirectoryPath = (path.join(__dirname, '../public'));
-const viewsDirectoryPath = (path.join(__dirname, '../templates'));
+const viewsDirectoryPath = path.join(__dirname, '../templates/views');
+const partialsDirectoryPath = path.join(__dirname, '../templates/partials');
 
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', viewsDirectoryPath);
-app.use(express.static(publicDirectoryPath));
 
+hbs.registerPartials(partialsDirectoryPath);
+
+app.use(express.static(publicDirectoryPath));
 app.use(morgan('common'));
 
 app.get('', (_req, res) => {
@@ -18,16 +22,15 @@ app.get('', (_req, res) => {
         title: 'Weather App',
         forecast: 'Guadalajara is partially cloudy',
         location: 'Guadalajara, Jalisco',
-        name: 'Ale',
     });
 });
 
 app.get('/about', (_req, res) => {
-    res.render('about', { name: 'Alessa' });
+    res.render('about', { title: 'About', name: 'Alessa' });
 });
 
 app.get('/help', (req, res) => {
-    res.render('help', { message: 'May I help you?' });
+    res.render('help', { title: 'Help', message: 'May I help you?' });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
