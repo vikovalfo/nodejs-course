d1035dbda8aec6d9ec25fab93913abc7c49c5374
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-21 14:45:32 -0600

    serving files for the view engine and parsing variables

--- 03_web-server/src/app.js
+++ 03_web-server/src/app.js
@@ -3,15 +3,26 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')));
+
 app.use(morgan('common'));
 
-app.get('/weather', (req, res) => {
-    res.send({
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
         forecast: 'Guadalajara is partially cloudy',
-        location: 'Guadalajara, Jalisco'
+        location: 'Guadalajara, Jalisco',
+        name: 'Ale',
     });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { name: 'Alessa' });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', { message: 'May I help you?' });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
