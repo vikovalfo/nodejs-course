3604488b7e7821716a4fe9a9fb88bdcf0fab4956
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-21 13:11:24 -0600

    serving static files

--- 03_web-server/src/app.js
+++ 03_web-server/src/app.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(morgan('common'));
-
-app.get('', (req, res) => {
-    res.send('hello express!');
-});
-
-app.get('/help', (req, res) => {
-    res.send('Help page');
-});
-
-app.get('/about', (req, res) => {
-    res.send('about page');
-});
 
 app.get('/weather', (req, res) => {
     res.send({
