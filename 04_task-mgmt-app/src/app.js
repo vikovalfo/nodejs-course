956d0da61ef53d5c11d0f71fb92464f6f0d124e0
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-23 13:33:28 -0600

    inserting data

--- 04_task-mgmt-app/src/app.js
+++ 04_task-mgmt-app/src/app.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const {connectionData} = require('./config');
+const { connectionData } = require('./config');
 
 const connectionURL = `${connectionData.protocol}${connectionData.user}${connectionData.password}${connectionData.hostname}${connectionData.domain}${connectionData.database}?retryWrites=true&w=majority`;
 
@@ -13,4 +13,31 @@ MongoClient.connect(connectionURL, {
     }
 
     console.log(`Connected to ${connectionData.database}`);
+
+    const db = client.db(connectionData.database)
+
+/*     db.collection('users').insertOne({
+        name: 'David',
+        age: 27
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user');
+        }
+
+        console.log(result.ops);
+    });
+
+    db.collection('users').insertMany([
+        { name: 'Jen', age: 21 },
+        { name: 'Robert', age: 32 }
+    ], (_error, result) => console.log(result.ops));
+
+    db.collection('tasks').insertMany([
+        { description: 'Clean the house', completed: true },
+        { description: 'Renew inspection', completed: false },
+        { description: 'Pot plants', completed: false },
+    ], (error, result) => {
+        if (error) return console.log('Unable to insert records');
+        console.log(result.ops);
+    }); */
 });
