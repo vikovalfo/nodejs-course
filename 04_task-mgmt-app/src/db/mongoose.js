1c794dcb60ca2f661b60d89b9c6239392de056b1
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-27 13:05:38 -0600

    validations on mongoose models

--- 04_task-mgmt-app/src/db/mongoose.js
+++ 04_task-mgmt-app/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const { connectionData } = require('../config');
 
 const connectionURL = `${connectionData.protocol}${connectionData.user}${connectionData.password}${connectionData.hostname}${connectionData.domain}${connectionData.database}`;
@@ -9,32 +10,70 @@ mongoose.connect(connectionURL)
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        default: '',
+        trim: true,
+        required: true,
+        validate(value) {
+            if (value.length < 6 || value.includes('password')) {
+                throw new Error('Password format entered is incorrect')
+            }
+        }
     }
-});
+})
 
 const me = new User({
-    name: 'Casssandra',
-    age: 27
+    name: '   Pepe  ',
+    email: 'myemail@gmail.uk  ',
+    password: 'asfkajsfsldfba'
+    //age:-1
 });
 
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+});
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 });
 
 const toDo = new Task({
     description: 'Should rip the ubuntu iso on the pendrive',
-    completed: true
 });
 
-// me.save().then(() => console.log(me)).catch((err) => console.log('Error saving the user: ', err));
-// toDo.save().then(() => console.log(toDo)).catch((err) => console.log('Error saving the task: ', err));
+toDo.save().then(() => console.log(toDo)).catch((err) => console.log('Error saving the task: ', err));
