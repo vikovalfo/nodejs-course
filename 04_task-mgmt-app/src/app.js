9aa62bf61171a01d47069401ccdd81715ffd6da2
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-27 16:24:06 -0600

    create user and task ep

--- 04_task-mgmt-app/src/app.js
+++ 04_task-mgmt-app/src/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const User = require('./models/user')
+const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.post('/users', (req, res) => {
-    const user = new User(req.body)
+    const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user)
+        res.status(200).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(200).send(task);
     }).catch((e) => {
         res.status(400).send(e)
     })
---
+++ 04_task-mgmt-app/src/models/task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectionURL = require('../config/connection');
+
+mongoose.connect(connectionURL);
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = Task;
