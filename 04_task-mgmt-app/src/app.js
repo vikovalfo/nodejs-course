7aa7f683847e685d9beb541a9b7bba3ab55b1fa0
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-11-28 15:04:32 -0600

    update task ep added

--- 04_task-mgmt-app/src/app.js
+++ 04_task-mgmt-app/src/app.js
@@ -93,4 +93,27 @@ app.get('/tasks/:id', async (req, res) => {
     }
 });
 
+app.patch('/tasks/:id', async (req, res) => {
+    const allowedUpdates = ['description', 'completed'];
+    const updates = Object.keys(req.body);
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+    
+    if (!isValidOperation) {
+        return res.send({ error: 'Invalid updates!' });
+    }
+
+    const _id = req.params.id;
+    const updatedFields = req.body;
+
+    try {
+        const updatedTask = await Task.findByIdAndUpdate({ _id }, { ...updatedFields }, { new: true, runValidators: true });
+        if (!updatedTask) {
+            return res.status(404).send();
+        }
+        res.status(200).send(updatedTask);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
 app.listen(port, () => console.log('Server listening on port', port));
